9a25c3bf74c1c28380e8b68e4627df39ff8107b0
Tilen Hribar <hribar.tilen@gmail.com>
2025-12-01 19:31:04 +0100

    Pofiksu kao upam

--- marketzone/store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product, Cart, CartItem
-
-# Registriramo modele, da jih vidimo v adminu
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(CartItem)
